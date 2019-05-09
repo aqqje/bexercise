b7d899dd06225b6cca2111ed3ecbf8115ce7c22f
aqqje <1042136232@qq.com>
2019-05-10 00:01:47 +0800

    nowcoder:swordmeansoffer add ==> solution27

---
+++ nowcoder/src/swordmeansoffer/Solution27_1.java
@@ -0,0 +1,40 @@
+package swordmeansoffer;
+
+
+import swordmeansoffer.base.TreeNode;
+
+import java.util.Stack;
+
+/**
+ * @author AqqJe
+ * @description 输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的双向链表。要求不能创建任何新的结点，只能调整树中结点指针的指向。
+ */
+public class Solution27_1 {
+
+    public TreeNode ConvertBSTToBiList(TreeNode root) {
+        if(root==null)
+            return null;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode p = root;
+        TreeNode pre = null;// 用于保存中序遍历序列的上一节点
+        boolean isFirst = true;
+        while(p!=null||!stack.isEmpty()){
+            while(p!=null){
+                stack.push(p);
+                p = p.left;
+            }
+            p = stack.pop();
+            if(isFirst){
+                root = p;// 将中序遍历序列中的第一个节点记为root
+                pre = root;
+                isFirst = false;
+            }else{
+                pre.right = p;
+                p.left = pre;
+                pre = p;
+            }
+            p = p.right;
+        }
+        return root;
+    }
+}
