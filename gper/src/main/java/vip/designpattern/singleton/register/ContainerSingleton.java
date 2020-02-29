faabd2ba90d2510d27f55d36cf2312e7de8b2724
aqqje <1042136232@qq.com>
2020-02-29 20:32:25 +0800

    gper:designpattern add ==> signleton1

--- gper/src/main/java/vip/designpattern/singleton/register/ContainerSingleton.java
+++ gper/src/main/java/vip/designpattern/singleton/register/ContainerSingleton.java
@@ -1,6 +1,5 @@
 package vip.designpattern.singleton.register;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -9,10 +8,12 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class ContainerSingleton {
 
-    private ContainerSingleton(){};
+    /** 处理被反射破坏单例 */
+    private ContainerSingleton(){throw new RuntimeException("不可被实例化");};
 
     private static Map<String, Object> ioc = new ConcurrentHashMap<String, Object>();
 
+    /** 线程不安全*/
     public static Object getInstance(String className){
         Object instance = null;
         if (!ioc.containsKey(className)) {
@@ -26,6 +27,40 @@ public class ContainerSingleton {
         }else{
             return ioc.get(className);
         }
+    }
+
+    /** 使用双重检查锁处理线程安全*/
+    public static Object getInstance2(String className){
+        Object instance = null;
+        if (!ioc.containsKey(className)) {
+            synchronized (ContainerSingleton.class){
+                if (!ioc.containsKey(className)) {
+                    try {
+                        instance = Class.forName(className).newInstance();
+                        ioc.put(className, instance);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    return instance;
+                }else{
+                    return ioc.get(className);
+                }
+            }
+        }
+        return ioc.get(className);
+    }
+
+    /** 使用putIfAbsent方法本身处理线程安全*/
+    public static Object getInstance3(String className){
+        Object instance = null;
+        if (!ioc.containsKey(className)) {
+            try {
+                ioc.putIfAbsent(className, instance);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return ioc.get(className);
 
     }
 }
