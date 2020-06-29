b62b856997d0121cb2fa010cee2950e1bc2b15fe
aqqje <1042136232@qq.com>
2020-06-29 17:08:12 +0800

    gper-springboot=> add spring-boot-demo

---
+++ gper-springboot/spring-boot-demo/src/main/java/com/aqqje/springbootdemo/HelloController.java
@@ -0,0 +1,14 @@
+package com.aqqje.springbootdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/test")
+    public String hello(){
+        return "hello!";
+    }
+}
