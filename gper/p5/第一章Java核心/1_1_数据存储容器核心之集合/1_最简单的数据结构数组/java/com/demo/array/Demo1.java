cb1b07967aad6acdbefd635696ca4d3dbe5becc5
aqqje <1042136232@qq.com>
2021-03-21 20:03:43 +0800

    update: gper-p5

---
+++ gper/p5/第一章Java核心/1_1_数据存储容器核心之集合/1_最简单的数据结构数组/java/com/demo/array/Demo1.java
@@ -0,0 +1,12 @@
+package com.demo.array;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        char[] cs = new char[]{'G','U','P','A','o'};
+        System.out.println(cs[5]);
+        /*
+        * Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: 5
+	at com.demo.array.Demo1.main(Demo1.java:6)
+        * */
+    }
+}
