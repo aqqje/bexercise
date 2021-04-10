1f867f98b69782119c030586cc26a2f05f7352a3
aqqje <1042136232@qq.com>
2021-04-10 20:38:25 +0800

    update: gper-p6

--- gper/p6/第五期/第二章架构师内功心法/2_2_设计模式/5_深度分析代理模式/代码/java/com/demo/pattern/proxy/dynamicproxy/jdkproxy/Test.java
+++ gper/p6/第五期/第二章架构师内功心法/2_2_设计模式/5_深度分析代理模式/代码/java/com/demo/pattern/proxy/dynamicproxy/jdkproxy/Test.java
@@ -14,7 +14,7 @@ public class Test {
 
         // 输出代理对象
         byte[] $Proxy0s = ProxyGenerator.generateProxyClass("$Proxy0", new Class[]{IPerson.class});
-        FileOutputStream fos = new FileOutputStream("/Users/aqqje/AqqJe/$Proxy0.class");
+        FileOutputStream fos = new FileOutputStream("c:/AqqJe/$Proxy0.class");
         fos.write($Proxy0s);
 
         IPerson zhaoLiu = jdkMeipo.getInstance(new ZhaoLiu());
